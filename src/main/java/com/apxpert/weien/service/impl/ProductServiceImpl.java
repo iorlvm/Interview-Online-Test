04a89cec86534b02a3f46d0cb6a27222013b7027
WeiEN <iorlvm001@gmail.com>
2024-10-09 10:47:21 +0800

    想到一個設計可以解決庫存問題  雖然測驗時間結束了  但還是想把他補完整

--- src/main/java/com/apxpert/weien/service/impl/ProductServiceImpl.java
+++ src/main/java/com/apxpert/weien/service/impl/ProductServiceImpl.java
@@ -3,25 +3,56 @@ package com.apxpert.weien.service.impl;
 import com.apxpert.weien.dao.ProductDao;
 import com.apxpert.weien.entity.Product;
 import com.apxpert.weien.service.ProductService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final static String PRODUCT_STOCK = "product:stock";
+
     @Autowired
     ProductDao productDao;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @PostConstruct
+    public void loadProductStockToRedis() {
+        // TODO: 將MQ中的隊列在啟動時完整處理並寫入RDB
+
+        // 預先將商品庫存載入redis中
+        List<Product> products = productDao.findAll();
+
+        // 將商品ID和庫存以 Hash 形式存入 Redis
+        for (Product product : products) {
+            stringRedisTemplate.opsForHash().put(PRODUCT_STOCK,
+                    String.valueOf(product.getId()),
+                    String.valueOf(product.getStock()));
+        }
+    }
+
 
     @Override
     public Page<Product> findAll(Integer page, Integer size) {
-        return productDao.findAll(PageRequest.of(page, size));
+        Page<Product> products = productDao.findAll(PageRequest.of(page, size));
+        // TODO: 從redis中讀取最新的庫存狀態
+        return products;
     }
 
     @Override
     public Product findById(Integer productId) {
-        return productDao.findById(productId).orElse(null);
+        Product product = productDao.findById(productId).orElse(null);
+        if (product != null) {
+            // TODO: 從redis中讀取最新的庫存狀態
+        }
+        return product;
     }
 
     @Override
@@ -38,6 +69,7 @@ public class ProductServiceImpl implements ProductService {
         }
         if (product.getStock() != null) {
             if (product.getStock() < 0) throw new IllegalArgumentException("庫存不得為負數");
+            // TODO: 修改為更新redis中的庫存 再將庫存更新放到MQ的尾端, 這個時間點不更新
             productFromDb.setStock(product.getStock());
         }
         if (product.getUnit() != null) {
@@ -55,6 +87,7 @@ public class ProductServiceImpl implements ProductService {
     public void deleteById(Integer productId) {
         Product product = productDao.findById(productId).orElseThrow(() -> new IllegalArgumentException("參數錯誤: 不存在的商品"));
         productDao.delete(product);
+        // TODO: 同時刪除redis中的資料
     }
 
     @Override
@@ -80,6 +113,8 @@ public class ProductServiceImpl implements ProductService {
             product.setImgSrc("http://fakeimg.pl/150x150"); // 未設定時給予預設值
         }
 
-        return productDao.save(product);
+        Product saved = productDao.save(product);
+        // TODO: 新增庫存到redis中
+        return saved;
     }
 }
